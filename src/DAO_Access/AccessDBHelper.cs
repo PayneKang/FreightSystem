ebe81859ffdbe9a7785c39c6c7cef1355750a38b
Payne Kang <daitou0322@tom.com>
2013-11-09 00:51:45 -0800

    Auto close db connection when exception raised in db

--- src/DAO_Access/AccessDBHelper.cs
+++ src/DAO_Access/AccessDBHelper.cs
@@ -40,41 +40,59 @@ namespace DAO_Access
         public DataSet ExecuteSql2DataSet(string sql, OleDbParameter[] parameters ,int startIndex , int length ,string srcTable)
         {
             OpenDB();
-            OleDbCommand cmd = new OleDbCommand(sql, conn);
-            if(parameters != null)
-                cmd.Parameters.AddRange(parameters);
-            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
-            DataSet ds = new DataSet();
-            if(length == 0 || string.IsNullOrEmpty(srcTable))
-                da.Fill(ds);
-            else
-                da.Fill(ds,startIndex,length,srcTable);
-            CloseDB();
-            return ds;
+            try
+            {
+                OleDbCommand cmd = new OleDbCommand(sql, conn);
+                if (parameters != null)
+                    cmd.Parameters.AddRange(parameters);
+                OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                if (length == 0 || string.IsNullOrEmpty(srcTable))
+                    da.Fill(ds);
+                else
+                    da.Fill(ds, startIndex, length, srcTable);
+                return ds;
+            }
+            finally
+            {
+                CloseDB();
+            }
         }
         public int ExecuteNonQuery(string sql,OleDbParameter[] parameters = null)
         {
             OpenDB();
-            OleDbCommand cmd = conn.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = sql;
-            if(parameters!=null)
-                cmd.Parameters.AddRange(parameters);
-            int count = cmd.ExecuteNonQuery();
-            CloseDB();
-            return count;
+            try
+            {
+                OleDbCommand cmd = conn.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = sql;
+                if (parameters != null)
+                    cmd.Parameters.AddRange(parameters);
+                int count = cmd.ExecuteNonQuery();
+                return count;
+            }
+            finally
+            {
+                CloseDB();
+            }
         }
         public object ExecuteScalar(string sql, OleDbParameter[] parameters = null)
         {
             OpenDB();
-            OleDbCommand cmd = conn.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = sql;
-            if (parameters != null)
-                cmd.Parameters.AddRange(parameters);
-            object result = cmd.ExecuteScalar();
-            CloseDB();
-            return result;
+            try
+            {
+                OleDbCommand cmd = conn.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = sql;
+                if (parameters != null)
+                    cmd.Parameters.AddRange(parameters);
+                object result = cmd.ExecuteScalar();
+                return result;
+            }
+            finally
+            {
+                CloseDB();
+            }
         }
     }
 }
